627fa7ac7038d8b5ad41c7b4c3554689b16c02c5
Mariemhachichi <hchichimariem@hotmail.fr>
2023-07-21 16:39:33 +0100

    rechercher

--- src/components/Produit.js
+++ src/components/Produit.js
@@ -1,31 +1,39 @@
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import { saveProduct } from '../App/app';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { saveProduct } from "../App/app";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Produit() {
-//Modal
-  const [show, setShow] = useState(false);
+  //Modal
+  const [show, setShow] = useState(false); 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-//
-const [Nom,setName] = useState("")
-const [Prix,setPrix] = useState(0)
-const [Quantité,setQuantité] = useState(0)
-const [checked,setCheked] = useState(false)
+  //Post
+  const [Nom, setName] = useState("");
+  const [Prix, setPrix] = useState(0);
+  const [Quantité, setQuantité] = useState(0);
+  const [checked, setCheked] = useState(false);
 
-const handleSaveProduct =(event)=>{
-    event.preventDefault()
-    let product = {Nom,Prix,Quantité,checked}
-    saveProduct(product).then((resp)=>{
-        alert(JSON.stringify(resp.data))
-    })
-}
-
+  const handleSaveProduct = (event) => {
+    event.preventDefault();
+    let product = { Nom, Prix, Quantité, checked };
+    saveProduct(product).then((resp) => {
+      console.log(JSON.stringify(resp.data));
+    });
+    setShow(false);
+    refrech();
+    alert("Enregistrement effectué avec succès!");
+  };
+  //Actualiser
+  const refrech = () => {
+    window.location.reload();
+  };
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
-        Ajouter produit
+        <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
       </Button>
 
       <Modal show={show} onHide={handleClose}>
@@ -33,33 +41,50 @@ const handleSaveProduct =(event)=>{
           <Modal.Title>Ajouter produit</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <form> 
-                <div className='md-3'>
-                <label className='Form-Label'>Nom</label> 
-                <input onChange={(e)=>setName(e.target.value)} value={Nom} className='form-control'></input>
-                </div>
-                <div className='md-3'>
-                <label className='Form-Label'>Prix unitaire</label> 
-                <input onChange={(e)=>setPrix(e.target.value)} value={Prix}  className='form-control'></input>
-                </div>
-                <div className='md-3'>
-                <label className='Form-Label'>Quantité</label> 
-                <input onChange={(e)=>setQuantité(e.target.value)} value={Quantité}  className='form-control'></input>
-                </div>
-                <div className="form-check">
-  <input onChange={(e)=>setCheked(e.target.value)} checked={checked}  className="form-check-input" type="checkbox" />
-  <label className="form-check-label" for="flexCheckCheked">
-    Checked 
-  </label>
-</div>
-            </form>
+          <form onSubmit={handleSaveProduct}>
+            <div className="md-3">
+              <label className="Form-Label">Nom</label>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={Nom}
+                className="form-control"
+              ></input>
+            </div>
+            <div className="md-3">
+              <label className="Form-Label">Prix unitaire</label>
+              <input
+                onChange={(e) => setPrix(e.target.value)}
+                value={Prix}
+                className="form-control"
+              ></input>
+            </div>
+            <div className="md-3">
+              <label className="Form-Label">Quantité</label>
+              <input
+                onChange={(e) => setQuantité(e.target.value)}
+                value={Quantité}
+                className="form-control"
+              ></input>
+            </div>
+            <div className="form-check">
+              <input
+                onChange={(e) => setCheked(e.target.value)}
+                checked={checked}
+                className="form-check-input"
+                type="checkbox"
+              />
+              <label className="form-check-label" for="flexCheckCheked">
+                Checked
+              </label>
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Annuler
           </Button>
-          <Button className="primary" onClick={handleSaveProduct}  >
-            Enregistrer 
+          <Button className="primary" onClick={handleSaveProduct}>
+            Enregistrer
           </Button>
         </Modal.Footer>
       </Modal>
--- src/components/Produits.js
+++ src/components/Produits.js
@@ -1,91 +1,120 @@
-import { faCheckCircle, faCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect, useState } from 'react'
-import { checkProduct, deleteProduct, getProducts } from '../App/app'
-import Produit from './Produit'
+import {
+  faCheckCircle,
+  faCircle,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { checkProduct, deleteProduct, getProducts } from "../App/app";
+import Produit from "./Produit";
+
 
 export default function Produits() {
-  const [products, setProducts] = useState([])
-
+  const [products, setProducts] = useState([]);
+  const [SearchProduct, SetSearchProduct] = useState("");
+  console.log(SearchProduct)
   //Get
-  useEffect(()=>{
-    handleGetProduct()
-  },[])
+  useEffect(() => {
+    handleGetProduct();
+  }, []);
 
-  const handleGetProduct =()=>{
-   getProducts().then(resp=>{
-    setProducts(resp.data)
-   }).catch(err=>{
-    console.log(err)
-   })
-  }
-   //Delete
-   const handleDeleteProduct =(product)=>{
-    deleteProduct(product).then((resp)=>{
-      const newProducts = products.filter((p) =>p.id != product.id)
-      setProducts(newProducts)
-    })
-   
-  }
+  const handleGetProduct = () => {
+    getProducts()
+      .then((resp) => {
+        setProducts(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  //Delete
+  const handleDeleteProduct = (product) => {
+    deleteProduct(product).then((resp) => {
+      const newProducts = products.filter((p) => p.id != product.id);
+      setProducts(newProducts);
+    });
+  };
   //Check Product
-  const handleCheckProduct =(product)=>{
+  const handleCheckProduct = (product) => {
+    checkProduct(product).then((resp) => {
+      const newProducts = products.map((p) => {
+        if (p.id == product.id) {
+          p.checked = !p.checked;
+        }
+        return p;
+      });
+      setProducts(newProducts);
+    });
+  };
+  //Rechercher 
 
-  checkProduct(product).then(resp=>{
-    const newProducts = products.map(p =>{
-      if(p.id == product.id){
-        p.checked=!p.checked
-      }
-      return p
-    })
-    setProducts(newProducts)
-  })
-  }
   return (
-    <div className='p-1 m-1'>
-      <div className='row'>
-        <div className='col-md-6'>
-          <div className='card'>
-            <div className='card-body'>
-                   <table className='table'>
-                    <thead>
-                      <tr>
-                      <th>ID</th>
-                      <th>Nom</th>
-                      <th>Prix unitaire</th>
-                      <th>Quantité</th>
-                      <th>Checked</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        products.map(product=>(
-                          <tr key={product.id}>
-                            <td>{product.id}</td>
-                            <td>{product.Nom}</td>
-                            <td>{product.Prix}</td>
-                            <td>{product.Quantité}</td>
-                            <td>
-                              <button onClick={()=>handleCheckProduct(product)} className='btn btn-outline-success'>
-                                <FontAwesomeIcon icon={product.checked ? faCheckCircle: faCircle}></FontAwesomeIcon>
-                              </button> 
-                            </td>
-                            <td>
-                              <button onClick={()=>handleDeleteProduct(product)} className='btn btn-outline-danger'>
-                                <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
-                              </button> 
-                            </td>
-                          </tr>
-                        ))
-                      }
-                    </tbody>
-                  </table>
+    <div className="p-1 m-1">
+      <div className="row">
+        <div className="col-md-">
+          <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="row">
+              <div className="col-auto">
+              <div className='row'>
+           <div className="col-auto">
+                  <input type='text' placeholder='Rechercher...' name='Rechercher' className="form-control" onChange={(e)=>SetSearchProduct(e.target.value)} ></input>
+            </div>
+    </div>
+               </div>
+              <div className="col-auto">
+              <Produit />
               </div>
+              </div>
+            </form>
+            </div>
+            <div className="card-body">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Prix unitaire</th>
+                    <th>Quantité</th>
+                    <th>Checked</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.filter((product)=>{
+                    return SearchProduct.toLowerCase() === '' ? product : product.Nom.toLowerCase().includes(SearchProduct)
+                  }).map((product) => (
+                    <tr key={product.id}>
+                      <td>{product.id}</td>
+                      <td>{product.Nom}</td>
+                      <td>{product.Prix}</td>
+                      <td>{product.Quantité}</td>
+                      <td>
+                        <button
+                          onClick={() => handleCheckProduct(product)}
+                          className="btn btn-outline-success"
+                        >
+                          <FontAwesomeIcon
+                            icon={product.checked ? faCheckCircle : faCircle}
+                          ></FontAwesomeIcon>
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => handleDeleteProduct(product)}
+                          className="btn btn-outline-danger"
+                        >
+                          <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
-          <div className='col-md-6'>
-                    <Produit />
-          </div>
-        </div>
+        </div> 
+      </div>
     </div>
-  )
+  );
 }
